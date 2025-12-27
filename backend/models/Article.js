3a61c98c73362f57335b48e48b5676d25d6994e1
antaseck27 <antalissa10@gmail.com>
2025-12-27 21:38:53 +0100

    Ajout de types

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -1,50 +1,25 @@
 
 const mongoose = require("mongoose");
 
-const ArticleSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  content: String,
-  image: String,
-
-  status: {
-    type: String,
-    enum: ["draft", "published"],
-    default: "published",
+const ArticleSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    content: String,
+    image: String,
+    status: {
+      type: String,
+      enum: ["draft", "published"],
+      default: "published",
+    },
+    likes: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
+    date: { type: Date, default: Date.now },
   },
-
-  likes: {
-    type: Number,
-    default: 0,
-  },
-
-  views: {
-    type: Number,
-    default: 0,
-  },
-
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true } // 🔥 TRÈS IMPORTANT
+);
 
 module.exports = mongoose.model("Article", ArticleSchema);
 
-
-// const mongoose = require("mongoose");
-
-// const articleSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   date: { type: String, required: true },
-//   description: { type: String },
-//   image: { type: String, default: "" },
-//   content: { type: String },
-//   likes: { type: Number, default: 0 },
-//   // ✅ AJOUT
-//   views: { type: Number, default: 0 },
-// });
-
-// module.exports = mongoose.model("Article", articleSchema);
 
 
